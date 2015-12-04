1ded05f383c72eeb35418932e5de2349ac4ed235
Zac McCormick <zac.mccormick@gmail.com>
2015-12-03 22:36:37 -0500

    Implement comparisons on numeric fields

--- test/elements/text-element.js
+++ test/elements/text-element.js
@@ -130,26 +130,82 @@ describe('text fields', () => {
     value.columnValue.should.eql(13.2);
 
     value.numericValue.should.eql(13.2);
+  });
+
+  it('implements comparisons for integer numeric fields', () => {
+    const value = record.formValues.find('integer_number');
+
+    value.textValue.should.eql('12');
+
+    // conditions
+    value.isEqual('12').should.eql(true);
+    value.isEqual('12.0').should.eql(false); // TODO(zhm) seems wrong
+    value.isEqual('11').should.eql(false);
+    value.isEqual(null).should.eql(false);
+
+    value.contains('1').should.eql(true);
+    value.contains('12').should.eql(true);
+    value.contains('12.2').should.eql(false);
+    value.contains('12.20').should.eql(false);
+    value.contains('a').should.eql(false);
+    value.contains(null).should.eql(false);
+
+    value.startsWith('1').should.eql(true);
+    value.startsWith('12').should.eql(true);
+    value.startsWith('12.3').should.eql(false);
+    value.startsWith('13.30').should.eql(false);
+    value.startsWith('a').should.eql(false);
+    value.startsWith(null).should.eql(false);
+
+    value.isLessThan('13').should.eql(true);
+    value.isLessThan('12.1').should.eql(true);
+    value.isLessThan('12').should.eql(false);
+    value.isLessThan('12.0').should.eql(false);
+    value.isLessThan('11.9').should.eql(false);
+    value.isLessThan('a').should.eql(false);
+    value.isLessThan(null).should.eql(false);
+
+    value.isGreaterThan('11').should.eql(true);
+    value.isGreaterThan('11.9').should.eql(true);
+    value.isGreaterThan('12').should.eql(false);
+    value.isGreaterThan('12.0').should.eql(false);
+    value.isGreaterThan('12.1').should.eql(false);
+    value.isGreaterThan('14').should.eql(false);
+    value.isGreaterThan('a').should.eql(false);
+    value.isGreaterThan(null).should.eql(false);
+  });
+
+  it('implements comparisons for decimal numeric fields', () => {
+    const value = record.formValues.find('decimal_number');
+
+    value.textValue.should.eql('13.2');
 
     // conditions
     value.isEqual('13.2').should.eql(true);
+    value.isEqual('13.20').should.eql(false); // TODO(zhm) seems wrong
     value.isEqual('12').should.eql(false);
     value.isEqual(null).should.eql(false);
 
     value.contains('13').should.eql(true);
+    value.contains('13.2').should.eql(true);
+    value.contains('13.20').should.eql(false);
     value.contains('a').should.eql(false);
     value.contains(null).should.eql(false);
 
     value.startsWith('13').should.eql(true);
+    value.startsWith('13.2').should.eql(true);
+    value.startsWith('13.20').should.eql(false);
     value.startsWith('a').should.eql(false);
     value.startsWith(null).should.eql(false);
 
     value.isLessThan('14').should.eql(true);
+    value.isLessThan('13.2').should.eql(false);
     value.isLessThan('12').should.eql(false);
     value.isLessThan('a').should.eql(false);
     value.isLessThan(null).should.eql(false);
 
     value.isGreaterThan('12').should.eql(true);
+    value.isGreaterThan('13.2').should.eql(false);
     value.isGreaterThan('14').should.eql(false);
     value.isGreaterThan('a').should.eql(false);
     value.isGreaterThan(null).should.eql(false);
