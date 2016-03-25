a941823086d0187b48e624f4f84c00bc15bb7217
Zac McCormick <zac.mccormick@gmail.com>
2016-03-25 17:37:27 -0400

    Add some role methods

--- dist/role.js
+++ dist/role.js
@@ -2,17 +2,44 @@
 
 exports.__esModule = true;
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Role = function Role(attributes) {
-  _classCallCheck(this, Role);
+var Role = function () {
+  function Role(attributes) {
+    _classCallCheck(this, Role);
 
-  attributes = attributes || {};
+    attributes = attributes || {};
 
-  this.id = attributes.id;
-  this.name = attributes.name;
-  this._attributes = attributes;
-};
+    this.id = attributes.id;
+    this.name = attributes.name;
+    this._attributes = attributes;
+
+    this._canChangeStatus = !!attributes.can_change_status;
+    this._canChangeProject = !!attributes.can_change_project;
+    this._canChangeAssignment = !!attributes.can_assign_records;
+  }
+
+  _createClass(Role, [{
+    key: "canChangeStatus",
+    get: function get() {
+      return this._canChangeStatus;
+    }
+  }, {
+    key: "canChangeProject",
+    get: function get() {
+      return this._canChangeProject;
+    }
+  }, {
+    key: "canChangeAssignment",
+    get: function get() {
+      return this._canChangeAssignment;
+    }
+  }]);
+
+  return Role;
+}();
 
 exports.default = Role;
 //# sourceMappingURL=role.js.map
--- src/role.js
+++ src/role.js
@@ -5,5 +5,21 @@ export default class Role {
     this.id = attributes.id;
     this.name = attributes.name;
     this._attributes = attributes;
+
+    this._canChangeStatus = !!attributes.can_change_status;
+    this._canChangeProject = !!attributes.can_change_project;
+    this._canChangeAssignment = !!attributes.can_assign_records;
+  }
+
+  get canChangeStatus() {
+    return this._canChangeStatus;
+  }
+
+  get canChangeProject() {
+    return this._canChangeProject;
+  }
+
+  get canChangeAssignment() {
+    return this._canChangeAssignment;
   }
 }
