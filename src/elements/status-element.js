4c845d0f2218e347ae2e8b64cf394c22cee8f4fa
Zac McCormick <zac.mccormick@gmail.com>
2016-04-04 09:25:06 -0400

    Make sure the status field exists on all forms.

--- src/elements/status-element.js
+++ src/elements/status-element.js
@@ -1,20 +1,31 @@
 import TextualElement from './textual-element';
 import StatusChoice from './status-choice';
 
+const DEFAULT_STATUS_ELEMENT = {
+  label: 'Status',
+  key: '@status',
+  data_name: 'status',
+  enabled: false,
+  read_only: false,
+  choices: []
+};
+
 export default class StatusElement extends TextualElement {
   constructor(parent, attributes) {
     attributes.type = 'StatusField';
 
-    super(parent, attributes);
+    const attrs = Object.assign({}, DEFAULT_STATUS_ELEMENT, attributes);
+
+    super(parent, attrs);
 
     this.choices = [];
 
-    for (const choice of attributes.choices) {
+    for (const choice of attrs.choices) {
       this.choices.push(new StatusChoice(choice));
     }
 
-    this._enabled = !!attributes.enabled;
-    this._readOnly = !!attributes.read_only;
+    this._enabled = !!attrs.enabled;
+    this._readOnly = !!attrs.read_only;
   }
 
   get isEnabled() {
