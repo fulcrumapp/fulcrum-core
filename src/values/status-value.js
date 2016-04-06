09c8a79d7d0b071542dcc835cf0303beb4fedb32
Zac McCormick <zac.mccormick@gmail.com>
2016-04-05 21:09:59 -0400

    Fix displayValue on status field

--- src/values/status-value.js
+++ src/values/status-value.js
@@ -1,4 +1,13 @@
 import TextualValue from './textual-value';
 
 export default class StatusValue extends TextualValue {
+  get displayValue() {
+    const choice = this.element.statusForValue(this.textValue);
+
+    return choice ? choice.label : this.textValue || '';
+  }
+
+  get searchableValue() {
+    return this.displayValue;
+  }
 }
