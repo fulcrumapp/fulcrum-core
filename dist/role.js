f39d9cfc1ef271f393b93c96c2395853c3ab3d93
Zac McCormick <zac.mccormick@gmail.com>
2017-04-06 11:15:52 -0400

    Add the rest of the role properties

--- dist/role.js
+++ dist/role.js
@@ -16,16 +16,84 @@ var Role = function () {
     this.name = attributes.name;
     this._attributes = attributes;
 
+    this._canManageSubscription = !!attributes.can_manage_subscription;
+    this._canUpdateOrganization = !!attributes.can_update_organization;
+    this._canManageMembers = !!attributes.can_manage_members;
+    this._canManageRoles = !!attributes.can_manage_roles;
+    this._canManageLayers = !!attributes.can_manage_layers;
+    this._canManageApps = !!attributes.can_manage_apps;
+    this._canManageProjects = !!attributes.can_manage_projects;
+    this._canManageChoiceLists = !!attributes.can_manage_choice_lists;
+    this._canManageClassificationSets = !!attributes.can_manage_classification_sets;
     this._canChangeStatus = !!attributes.can_change_status;
     this._canChangeProject = !!attributes.can_change_project;
+    this._canChangeLocation = !!attributes.can_change_location;
     this._canChangeAssignment = !!attributes.can_assign_records;
     this._canRunReports = !!attributes.can_run_reports;
     this._canCreateRecords = !!attributes.can_create_records;
     this._canUpdateRecords = !!attributes.can_update_records;
     this._canDeleteRecords = !!attributes.can_delete_records;
+    this._canExportRecords = !!attributes.can_export_records;
+    this._canImportRecords = !!attributes.can_import_records;
+    this._canManageAuthorizations = !!attributes.can_manage_authorizations;
   }
 
   _createClass(Role, [{
+    key: "canManageSubscription",
+    get: function get() {
+      return this._canManageSubscription;
+    }
+  }, {
+    key: "canUpdateOrganization",
+    get: function get() {
+      return this._canUpdateOrganization;
+    }
+  }, {
+    key: "canManageMembers",
+    get: function get() {
+      return this._canManageMembers;
+    }
+  }, {
+    key: "canManageRoles",
+    get: function get() {
+      return this._canManageRoles;
+    }
+  }, {
+    key: "canManageLayers",
+    get: function get() {
+      return this._canManageLayers;
+    }
+  }, {
+    key: "canManageApps",
+    get: function get() {
+      return this._canManageApps;
+    }
+  }, {
+    key: "canManageProjects",
+    get: function get() {
+      return this._canManageProjects;
+    }
+  }, {
+    key: "canManageChoiceLists",
+    get: function get() {
+      return this._canManageChoiceLists;
+    }
+  }, {
+    key: "canManageClassificationSets",
+    get: function get() {
+      return this._canManageClassificationSets;
+    }
+  }, {
+    key: "canChangeLocation",
+    get: function get() {
+      return this._canChangeLocation;
+    }
+  }, {
+    key: "canImportRecords",
+    get: function get() {
+      return this._canImportRecords;
+    }
+  }, {
     key: "canChangeStatus",
     get: function get() {
       return this._canChangeStatus;
@@ -54,6 +122,11 @@ var Role = function () {
     key: "canDeleteRecords",
     get: function get() {
       return this._canDeleteRecords;
+    }
+  }, {
+    key: "canExportRecords",
+    get: function get() {
+      return this._canExportRecords;
     }
   }, {
     key: "canRunReports",
--- src/role.js
+++ src/role.js
@@ -6,13 +6,70 @@ export default class Role {
     this.name = attributes.name;
     this._attributes = attributes;
 
+    this._canManageSubscription = !!attributes.can_manage_subscription;
+    this._canUpdateOrganization = !!attributes.can_update_organization;
+    this._canManageMembers = !!attributes.can_manage_members;
+    this._canManageRoles = !!attributes.can_manage_roles;
+    this._canManageLayers = !!attributes.can_manage_layers;
+    this._canManageApps = !!attributes.can_manage_apps;
+    this._canManageProjects = !!attributes.can_manage_projects;
+    this._canManageChoiceLists = !!attributes.can_manage_choice_lists;
+    this._canManageClassificationSets = !!attributes.can_manage_classification_sets;
     this._canChangeStatus = !!attributes.can_change_status;
     this._canChangeProject = !!attributes.can_change_project;
+    this._canChangeLocation = !!attributes.can_change_location;
     this._canChangeAssignment = !!attributes.can_assign_records;
     this._canRunReports = !!attributes.can_run_reports;
     this._canCreateRecords = !!attributes.can_create_records;
     this._canUpdateRecords = !!attributes.can_update_records;
     this._canDeleteRecords = !!attributes.can_delete_records;
+    this._canExportRecords = !!attributes.can_export_records;
+    this._canImportRecords = !!attributes.can_import_records;
+    this._canManageAuthorizations = !!attributes.can_manage_authorizations;
+  }
+
+  get canManageSubscription() {
+    return this._canManageSubscription;
+  }
+
+  get canUpdateOrganization() {
+    return this._canUpdateOrganization;
+  }
+
+  get canManageMembers() {
+    return this._canManageMembers;
+  }
+
+  get canManageRoles() {
+    return this._canManageRoles;
+  }
+
+  get canManageLayers() {
+    return this._canManageLayers;
+  }
+
+  get canManageApps() {
+    return this._canManageApps;
+  }
+
+  get canManageProjects() {
+    return this._canManageProjects;
+  }
+
+  get canManageChoiceLists() {
+    return this._canManageChoiceLists;
+  }
+
+  get canManageClassificationSets() {
+    return this._canManageClassificationSets;
+  }
+
+  get canChangeLocation() {
+    return this._canChangeLocation;
+  }
+
+  get canImportRecords() {
+    return this._canImportRecords;
   }
 
   get canChangeStatus() {
@@ -37,6 +94,10 @@ export default class Role {
 
   get canDeleteRecords() {
     return this._canDeleteRecords;
+  }
+
+  get canExportRecords() {
+    return this._canExportRecords;
   }
 
   get canRunReports() {
