3d7b804288398024c861b1f984df99840de6ca13
Jirles <emily.jirles@gmail.com>
2020-01-06 17:02:06 -0500

    Remove comments

--- src/elements/project-element.js
+++ src/elements/project-element.js
@@ -17,23 +17,9 @@ export default class ProjectElement extends TextualElement {
 
     super(parent, attrs);
 
-    // this._projectFilter = null;
-    // this._choices = [];
-
-    // if (attrs.choices) {
-    //   for (const choice of attrs.choices) {
-    //     this._choices.push(choice);
-    //   }
-    // }
-
     this._disabled = !!attrs.disabled;
     this._hidden = !!attrs.hidden;
   }
-
-  // get choices() {
-  //   // return this.filteredChoices;
-  //   return this._choices;
-  // }
 
   get isEnabled() {
     return !this._disabled;
@@ -42,14 +28,6 @@ export default class ProjectElement extends TextualElement {
   get isReadOnly() {
     return this._overrideIsDisabled != null ? this._overrideIsDisabled : this._readOnly;
   }
-
-//   get statusFilter() {
-//     return this._statusFilter;
-//   }
-
-//   set statusFilter(statusFilter) {
-//     this._statusFilter = statusFilter;
-//   }
 
   projectForValue(value) {
     for (const choice of this.choices) {
@@ -60,36 +38,4 @@ export default class ProjectElement extends TextualElement {
 
     return null;
   }
-
-//   get filteredChoices() {
-//     const items = this._choices;
-
-//     if (!this.statusFilter) {
-//       return items;
-//     }
-
-//     const filteredItems = [];
-
-//     for (const item of items) {
-//       for (const filter of this.statusFilter) {
-//         if (item.value.toLowerCase().indexOf(filter.toLowerCase()) !== -1) {
-//           filteredItems.push(item);
-//         }
-//       }
-//     }
-
-//     return filteredItems;
-//   }
-
-//   get overrideValues() {
-//     return Object.assign(Object.getOwnPropertyDescriptor(Element.prototype, 'overrideValues').get.call(this), {
-//       statusFilter: this._statusFilter
-//     });
-//   }
-
-//   resetOverrides() {
-//     super.resetOverrides();
-
-//     this._statusFilter = null;
-//   }
 }
