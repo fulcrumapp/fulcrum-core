d15eb3a79de80fe44fbc14c41efcfd069e624d85
Kevin Osborn <osbornk@users.noreply.github.com>
2022-12-05 18:34:21 -0600

    FLCRM-9851 Fix spacing

--- src/elements/button-element.js
+++ src/elements/button-element.js
@@ -1,9 +1,9 @@
 import Element from './element';
 
 export default class ButtonElement extends Element {
-    constructor(parent, attributes) {
-        super(parent, attributes);
-    
-        this.buttonType = attributes.button_type;
-      }
+  constructor(parent, attributes) {
+    super(parent, attributes);
+
+    this.buttonType = attributes.button_type;
+  }
 }
